c8939d0c8525545bd54f54ce9a07674e6c4b300e
chrishellon <chrisghellon@gmail.com>
2023-02-23 17:38:10 +0700

    Full implementation with travaloud base app

--- FuseHostelsAndTravel.Core/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace FuseHostelsAndTravel.Core.Models
-{
-    public class ApplicationUser : Travaloud.Core.Models.ApplicationUser
-    {
-        
-    }
-}
